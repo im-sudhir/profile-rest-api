ef997b14f26fcda62e16c475158e6150a1174903
im-sudhir <ssudhir794@gmail.com>
2024-12-25 01:28:41 +0530

    added login api

--- profiles_api/urls.py
+++ profiles_api/urls.py
@@ -8,7 +8,8 @@ router.register(r'hello-viewset', views.HelloViewSet, basename='hello-viewset')
 router.register(r'profile', views.UserProfileViewSet)
 
 urlpatterns=[
+    # path('hello-view/', views.HelloApiView.as_view()),
+    path('login/', views.UserLoginApiView.as_view()),
     path('', include(router.urls)),
-    path('hello-view/', views.HelloApiView.as_view()),
 
 ]
