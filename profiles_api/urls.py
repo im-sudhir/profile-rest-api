3ac21e64b201f1c233f7f940a3439de84914019b
im-sudhir <ssudhir794@gmail.com>
2024-12-25 00:31:42 +0530

    added profiles viewsets

--- profiles_api/urls.py
+++ profiles_api/urls.py
@@ -5,6 +5,7 @@ from rest_framework.routers import DefaultRouter
 
 router =DefaultRouter()
 router.register(r'hello-viewset', views.HelloViewSet, basename='hello-viewset')
+router.register(r'profile', views.UserProfileViewSet)
 
 urlpatterns=[
     path('', include(router.urls)),
